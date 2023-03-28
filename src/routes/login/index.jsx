773c9a82cdc19c2f7d91f08bc8902be36e33aa64
Sillas Vinícius Barbosa Braga <sillasvinicius0403@gmail.com>
2023-03-28 17:08:57 -0300

    ajustes tela de login

--- src/routes/login/index.jsx
+++ src/routes/login/index.jsx
@@ -8,14 +8,25 @@ export default function Login() {
     <div className='container'>
       <div className='main-content'>
         <div className='img-container'>
-          <h1>Login</h1>
-        </div>
-        <div className='img-container'>
           <img src={logo} alt="Logo" className='img-logo' />
+        </div>
+        <div className='input-container'>
+          <input type="text" className='text-input' name="nomeUsuario" id="nomeUsuario" placeholder="Nome"/>
+        </div>
+        <div className='input-container'>
+          <select name="perfilUsuario" id="perfilUsuario" className='select-input'>
+            <option value="" disabled selected hidden>Perfil</option>
+            <option value="1">PLAYER</option>
+            <option value="2">MODERADOR</option>
+          </select>
+        </div>
+        <div className='input-container'>
+          <input type="text" className='text-input' name="salaUsuario" id="salaUsuario" placeholder="Sala"/>
         </div>
         <div className='button-container'>
           <Link className='button-entry' to='/PLPoker'>Voltar</Link>
-          <Link className='button-entry' to='/PLPoker/Login'>Login</Link>
+          <Link className='button-entry' to='/PLPoker/Login'>Entrar</Link>
+          {/* <Link className='button-entry' to='/PLPoker/Login'>Criar Sala</Link> */}
         </div>
       </div>
     </div>
